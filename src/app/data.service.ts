87440de926ae83cf7da293cf27aec4a50f102643
yo0x <6yonor@gmail.com>
2021-05-22 19:48:13 +0000

    nokey

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Client } from './models/client.model';
 import { HttpClient } from '@angular/common/http';
+const https = require('https');
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-  powerlink_create_url = ''
+  powerlink_create_url = 'https://api.powerlink.co.il/api/record/account';
 
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  postContact(data: any){
+    this._http.post(this.powerlink_create_url,data,{headers: {
+      'Content-Type': 'application/json',
+      'tokenId': ''
+    }});
+    console.warn(data);
+  }
 }
