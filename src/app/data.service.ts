d2228ebbffe87fdbb7344a3a130600ec8b783864
yo0x <6yonor@gmail.com>
2021-06-21 18:43:45 +0000

    working on

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -4,23 +4,21 @@ import { Client } from './models/client.model';
 
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-
-
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  powerlink_create_url = 'http://localhost:5555';
-
+  // powerlink_create_url = 'https://api.powerlink.co.il/api/record/account';
+  powerlink_create_url = 'https://192.168.55.158:3636';
 
   constructor(private _http: HttpClient) { }
 
   postContact(data: any){
-    let headers = new HttpHeaders({
+    let headersToP = new HttpHeaders({
       'Content-Type': 'application/json',
       'tokenId': '51d889d9-7e7f-4529-8d53-5157865a887a'});
-      let options = { headers: headers };
+      let options = { headers: headersToP };
  
 
     this._http.post(this.powerlink_create_url,data,options);
