450ec872f16ab28bf081d7e52897895b7ef8a7fe
lior42 <liorlander@gmail.com>
2021-06-25 22:33:22 +0200

    added tailwind-css for later use

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./app/**/*.{ts, html, js}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
