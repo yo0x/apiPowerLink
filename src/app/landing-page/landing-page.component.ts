b6ad93b7968c7f7d4bd556f197fa3b495d03afc5
yo0x <6yonor@gmail.com>
2021-06-28 20:26:35 +0300

    ok

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -2,12 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Client } from '../models/client.model';
 import { DataService } from '../data.service';
-<<<<<<< HEAD
-import {FormsModule, NgForm} from '@angular/forms'
-
-=======
 import { NgForm } from '@angular/forms';
->>>>>>> a272da9947f67b75adf4cfd728fd1085334b681f
 
 
 
@@ -38,27 +33,12 @@ export class LandingPageComponent implements OnInit {
    "billingcity" : "tlv"
 }
 
-<<<<<<< HEAD
-  // onSubmit(data: any) {
-  //   this.http.post('https://api.powerlink.co.il/api/record/account', data, {
-  //     HttpHeaders: {
-  //       'Content-Type': 'application/json',
-  //       'tokenId': '51d889d9-7e7f-4529-8d53-5157865a887a'
-  //     }
-  //   }).subscribe((data) => {
-  //     console.warn(data);
-  //   });
-  // }
-  onSubmit(data: any, myForm : NgForm) {
-    this.dataService.postContact(data);
-=======
   onSubmit(data: any,myForm: NgForm) {
     
     const res = this.http.post('https://api.powerlink.co.il/api/record/account',this.data1,{
       // headers: headers1
     });
     console.info(res);
->>>>>>> a272da9947f67b75adf4cfd728fd1085334b681f
     myForm.reset();
   }
   // onSubmit(data: any, myForm: NgForm) {
